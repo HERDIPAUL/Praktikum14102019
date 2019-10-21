71484c4ecefbc227a10478453761e49345789d68
HERDIPAUL <56816010+HERDIPAUL@users.noreply.github.com>
2019-10-21 13:48:28 +0700

    Add files via upload

---
+++ percabangan.php
@@ -0,0 +1,22 @@
+<?php
+//$hari = 'senin';
+//if($hari == 'senin'){
+//	echo "praktikum semester 3";
+//}else if( $hari == 'selasa'){
+//	echo "kuliah";
+//}else{
+//	echo "waktunya libur";
+//}
+
+$tanggal = 10;
+
+switch($tanggal){
+	case 1:
+		echo "tanggal muda";
+		break;
+	case 27:
+		echo "tanggal tua, waktunya hemat";
+		break;
+	default:
+		echo "nanana";
+}
